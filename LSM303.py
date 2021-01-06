d5744b314581d0c7259bbeb2a36f7f539fc12ec4
HTunne <py13ht@leeds.ac.uk>
2021-01-06 16:51:22 +0000

    Add file for LSM303.

---
+++ LSM303.py
@@ -0,0 +1,14 @@
+
+class LSM303:
+"""Class to controll the LSM303 3D accelerometer and 3D magnetometer module."""
+    def __init__(self, I2C_object, address_LSb = 1):
+
+
+
+
+
+
+
+    #def set_mode_normal()
+    #def set_mode_low_power()
+
